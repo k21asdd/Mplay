a50153bfcec5c7f225f4c15647aa92cf3c95a2cc
Yuan-Chang <k21asdd@hotmail.com>
2015-07-27 16:44:27 +0800

    Modify search algorithm from rec to BFS

--- mp3player/MusicSearchThread.java
+++ mp3player/MusicSearchThread.java
@@ -10,28 +10,31 @@ import android.util.Log;
 
 
 public class MusicSearchThread extends Thread{
-	private File root;
 	private ArrayList<String> MuArrList;
+	private ArrayList<File> DirArrList;
 	MusicSearchThread(final File folder, ArrayList<String> songs){
-		root = folder;
 		MuArrList = songs;
+		DirArrList = new ArrayList<File>();
+		DirArrList.add(folder);
 	}
 	@Override
 	public void run(){
-		if(root.isFile())return;
-		recFindMusic(root);
-	}
-	private void recFindMusic(File f){
-		if(f.listFiles() == null) return;
-		for(File subF: f.listFiles()){
-			if(subF.isFile() && acceptMp3(subF)){
-				synchronized(MuArrList){
-					MuArrList.add(subF.getPath());
+		String dir = DirArrList.get(0).getName();
+		long start = System.currentTimeMillis();
+		while(DirArrList.size() > 0){
+			File f = DirArrList.remove(DirArrList.size()-1);
+			for(File subF: f.listFiles()){
+				if(subF.isFile() && acceptMp3(subF)){
+					synchronized(MuArrList){
+						MuArrList.add(subF.getPath());
+					}
+				}else if(subF.isDirectory()){
+					DirArrList.add(subF);
 				}
-			}else if(subF.isDirectory()){
-				recFindMusic(subF);
 			}
 		}
+		Log.d("Time", "Thread " + dir + " : " +
+				(System.currentTimeMillis() - start));
 	}
 	private boolean acceptMp3(File pathname) {
 		return pathname.toString().matches("[\\w0-9\\s\\S]+.mp3$");
